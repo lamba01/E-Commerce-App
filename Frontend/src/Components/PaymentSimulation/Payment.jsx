9ff3d083c99a65d15feafa33bf56f3db7b179d51
lamba01 <jamesspaul987@gmail.com>
2023-10-26 07:57:52 +0100

    simulated functionalities for payment

--- Frontend/src/Components/PaymentSimulation/Payment.jsx
+++ Frontend/src/Components/PaymentSimulation/Payment.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { AiFillCreditCard, AiOutlineBank, AiOutlineArrowLeft} from 'react-icons/ai'
+import { AiFillCreditCard, AiOutlineBank, AiOutlineArrowLeft, AiOutlineCheck, AiOutlineClose } from 'react-icons/ai'
 import { BsPaypal } from 'react-icons/bs'
 import { BiLogoVisa } from 'react-icons/bi'
 import { FcSimCardChip } from 'react-icons/fc'
@@ -16,35 +16,88 @@ const PaymentSimulation = () => {
     address: '',
     paymentMethod: '',
     cardNumber: '',
-    expirationDate: '',
+    expirationMonth: 1,
     cvv: '',
   });
+  const formRefs = [useRef(null), useRef(null)]; 
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+
+        if (name === 'cardNumber') {
+          const numericValue = value.replace(/[^0-9]/g, '');
+
+          // Update the input field with the numeric value
+          event.target.value = numericValue;
+
+          // Update the state with the cleaned numeric value
+          setFormData({ ...formData, [name]: numericValue });
+
+          // Update the span element with the formatted value
+          const formattedValue = numericValue.replace(/(\d{4})/g, '$1 ').trim();
+          // If the input field is for card number, update the span element
+          const cardNumberSpan = document.getElementById('card-number-span');
+          if (cardNumberSpan) {
+          cardNumberSpan.textContent = formattedValue;
+      }
+    }
+  };
+  const handleMonthChange = (event) => {
+    const { name, value } = event.target;
+    
+    // Ensure the value is a number and within the allowed range (1-12)
+    const numericValue = parseInt(value, 10);
+    if (!isNaN(numericValue) && numericValue >= 1 && numericValue <= 12) {
+      setFormData({ ...formData, [name]: numericValue });
+      const expMonth = document.getElementById('exp-month')
+      // Format the month with a leading zero if needed
+      const formattedMonth = numericValue.toString().padStart(2, '0');
+      expMonth.textContent = formattedMonth;
+    }
+  };
+  const handleKeyDown = (event) => {
+    if (
+      (event.key !== 'ArrowUp' && event.key !== 'ArrowDown') || 
+      (parseInt(event.target.value) === 1 && event.key === 'ArrowDown') ||
+      (parseInt(event.target.value) === 12 && event.key === 'ArrowUp')
+    ) {
+      event.preventDefault();
+    }
   };
 
-  const handleNextStep = (event) => {
-    event.preventDefault();
+  const handleYearChange = (event) => {
+    const { name, value } = event.target;
+  
+    // Ensure the value is a number
+    const numericValue = parseInt(value, 10);
+    if (!isNaN(numericValue)) {
+      setFormData({ ...formData, [name]: numericValue });
+      const expyear = document.getElementById('exp-year')
+      expyear.textContent = numericValue
+    }
+  };
+
+  const handleCardNameChange = (event) => {
+    const { name, value } = event.target;
+  
+      setFormData({ ...formData, [name]: value });
+      const holder = document.getElementById('holder-name')
+      holder.textContent = value
+    
+  };
+
+  const handleNextStep = () => {
+    // event.preventDefault();
     setStep(step + 1);
   };
 
   const handlePreviousStep = () => {
-    setStep(step - 1);
+    if(step === 2){
+      setStep(step - 1);
+    }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-      // Simulate a successful payment (you can add validation logic here)
-      handleNextStep();
-  };
-  const formRef = useRef();
-  const handleNextButtonClick = () => {
-    if (formRef.current) {
-      formRef.current.requestSubmit();
-    }
-  }
   const renderProgressBar = () => {
     const progressBarItems = ['1', '2', '3'];
   return (
@@ -64,16 +117,28 @@ const PaymentSimulation = () => {
   </div>
   );
 };
+const handleSubmit = (formIndex) => {
+  const form = formRefs[formIndex].current;
+
+  if (form.checkValidity()) {
+    // If the form is valid, proceed to the next step
+    handleNextStep();
+  } else {
+    // Handle validation errors or provide feedback to the user
+    form.reportValidity()
+  }
+};
 
 
   return (
     <div className="payment-simulation">
       <div className="payment-container">
-      <AiOutlineArrowLeft onClick={handlePreviousStep}/>
+      <div className='btns'><AiOutlineArrowLeft className='iis' size={'1.5em'} onClick={handlePreviousStep}/>
+      <AiOutlineClose size={'1.5em'} className='iis'/></div>
       <h2 className='paymentheader'>Make Payment</h2>
       {renderProgressBar()}
         <div className={`step ${step === 1 ? 'active' : ''}`}>       
-          <form className='forms' onSubmit={handleNextStep} ref={formRef}>
+          <form className='forms' ref={formRefs[0]}>
             <div>
               <input
               type="text"
@@ -150,7 +215,7 @@ const PaymentSimulation = () => {
             onChange={handleInputChange} id="" cols="30" rows="10"></textarea>
            </div>                    
           </form>
-          <button type="submit" onClick={handleNextButtonClick}>Go to Payment</button> 
+          <button onClick={() => handleSubmit(0)}>Go to Payment</button> 
         </div>
 
         <div className={`step ${step === 2 ? 'active' : ''}`}>
@@ -160,37 +225,58 @@ const PaymentSimulation = () => {
             <div className="logoss">
               <FcSimCardChip size={'2em'}/><BiLogoVisa size={'4em'}/></div>
               <div className="card-content">
-              <span className='card-number'>1234 5678 9012 3456 </span>
-              <div><h4 className='card-name'>John Doe</h4>
-              <p>00/00</p></div>
+              <span className='card-number' id="card-number-span" >1234 5678 9012 3456 </span>
+              <div className='card-content-sub'><h4 className='card-name' id='holder-name'>John Doe</h4>
+              <div className='expp'><p id='exp-month'>00</p>/<p id='exp-year'>0000</p></div></div>
             </div>
             </div>
             
           </div>
-          <form className='sec-form' onSubmit={handleSubmit}>
-            <div><input
+          <form className='sec-form' ref={formRefs[1]}>
+            <div>
+              <input
               type="text"
               name="cardNumber"
               className='cn'
+              pattern="[0-9]{16}"
+              minLength={16}
+              maxLength={16}
               placeholder="Card Number"
               value={formData.cardNumber}
               onChange={handleInputChange}
               required
             /></div>
-            <div className='aa'><input
-              type="text"
-              name="expirationDate"
-              className='exp'
-              placeholder="Expiry (MM/YY)"
-              value={formData.expirationDate}
-              onChange={handleInputChange}
+            <div className='aa'>
+            <input type="number"
+             maxLength={2}
+            max={12}
+            min={1}
+            placeholder='0'
+            value={formData.expirationMonth.toString().padStart(2, '0')}
+            onChange={handleMonthChange}
+            name="expirationMonth"
+            required
+            className='expm'
+            onKeyDown={handleKeyDown}
+            />
+            <input
+              type="number"
+              name="expirationYear"
+              placeholder="YYYY"
+              value={formData.expirationYear}
+              onChange={handleYearChange}
+              min={new Date().getFullYear()} // Minimum year is the current year
               required
+              className='expy'
+              onKeyDown={handleKeyDown}
             />
             <input
               type="text"
               name="cvv"
               className='cvv'
               placeholder="CVV"
+              pattern="[0-9]{3}"
+              maxLength={3}
               value={formData.cvv}
               onChange={handleInputChange}
               required
@@ -201,25 +287,29 @@ const PaymentSimulation = () => {
               name="holdername"
               className='hn'
               placeholder="Holder Name"
+              pattern="[A-Za-z ]+"
               value={formData.holdername}
-              onChange={handleInputChange}
+              onChange={handleCardNameChange}
               required
             />
             </div>
           </form>
-          </div>
-          {/* <button type="button" onClick={handlePreviousStep}>
-              Previous
-            </button> */}
-            <button type="submit">Pay Now</button>
+          </div>  
+          <button className='formm2' onClick={() => handleSubmit(1)}>Pay Now</button>    
         </div>
         <div className={`step ${step === 3 ? 'active' : ''}`}>
-          <h2>Step 3: Order Confirmation</h2>
+          <div className="check-container"><AiOutlineCheck size={'2em'} color='white'/></div>
+          <h2>Success</h2>
           <p>Thank you for your order! Your payment was successful.</p>
+          <button className='formm2'>Complete</button>
         </div>
       </div>
     </div>
   );
 };
+
+
+
+
 
 export default PaymentSimulation;
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -7,7 +7,6 @@ import ProductsList from "../Components/ProductsList";
 // import Navigation from "../Components/Navigation";
 import Products from "../Components/Products";
 import Test from "../Components/Test";
-import PaymentSimulation from "../Components/PaymentSimulation/Payment";
 // import Cart from "./Cart";
 // import { Router } from "react-router-dom";
 // import ProductDetails from "./ProductDetails";
@@ -94,7 +93,6 @@ function Home() {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <PaymentSimulation />
       <Test />
       <Products searchQuery={searchQuery} />
       <ProductsList searchQuery={searchQuery} products={filteredProducts} /> 
