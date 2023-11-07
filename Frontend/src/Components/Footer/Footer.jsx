20520c3921c051a1aaa53e38e3c541e68521c64f
lamba01 <jamesspaul987@gmail.com>
2023-11-07 21:58:59 +0100

    Designed footer

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLocation  } from 'react-router-dom';
+import { BiLogoFacebook } from 'react-icons/bi'
+import { RiTwitterXFill } from 'react-icons/ri'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { FaLinkedinIn } from 'react-icons/fa'
 import "./footer.css"
 
 const Footer = () => {
+    const location = useLocation();
+
+  const pathsToHideNavbar = ["/login", "/Signup"];
+
+  // Check if the current path is in the array of paths to hide the navbar
+  const hideNavbar = pathsToHideNavbar.includes(location.pathname);
   return (
-    <footer className="footer">
+    <div>{!hideNavbar && (<footer className="footer">
       <div className="container row">
         <div className="footer-col">
           <h4>company</h4>
@@ -36,14 +47,15 @@ const Footer = () => {
         <div className="footer-col">
           <h4>follow us</h4>
           <div className="social-links">
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"> </a>
+            <div>< BiLogoFacebook /></div>
+            <div><RiTwitterXFill /></div>
+            <div><AiOutlineInstagram /></div>
+            <div><FaLinkedinIn /></div>
           </div>
         </div>
       </div>
-    </footer>
+    </footer>)}
+    </div>
   );
 };
 
