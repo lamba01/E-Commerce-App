48d3f6ef4f73665fe5a5a8d372308b9322bf879a
lamba01 <jamesspaul987@gmail.com>
2023-11-13 22:20:56 +0100

    Designed shop page and added responsiveness

--- Backend/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  secretKey: "LkRj7&9sd^9~[9T$^a2Gy3zN7f]8TrpFgWz9xJ6qE0!KsUcN1vPwO5xR9aG3tZ",
-  db: {
-    host: "localhost",
-    user: "root",
-    password: "2004",
-    database: "myfirstdatabase",
-  },
-};
