90496eb6d33cfd6ccb43c2021b61779cf048a6ed
lamba01 <jamesspaul987@gmail.com>
2023-11-20 21:57:14 +0100

    CORS middleware

--- Server/index.js
+++ Server/index.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://commeercee.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
