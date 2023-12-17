8d43927438b416d4d8c25e0f2a89dafa609beba8
lamba01 <jamesspaul987@gmail.com>
2023-12-17 07:34:13 +0100

    added error handling to orders page

--- Client/src/Pages/Orders/Orders.jsx
+++ Client/src/Pages/Orders/Orders.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import "./orders.css"
 import Loading from '../../Components/LoadingAnimation/Loading';
 
@@ -9,12 +10,15 @@ const Orders = () => {
   const [error, setError] = useState(null);
   const token = localStorage.getItem('token');
   const apiUrl = 'https://app-chi-pink.vercel.app'; 
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
         if (!token) {
-          alert("Please log in!");
+          alert('You are not logged in. Please log in to see your order details.');
+          navigate('/login')
+          return;
         }
         const response = await axios.get(`${apiUrl}/api/orders`, {
           headers: {
@@ -30,7 +34,7 @@ const Orders = () => {
     };
 
     fetchOrders();
-  }, []); // Empty dependency array ensures the effect runs only once on component mount
+  }, [token, navigate]); 
 
   return (
     <div className='order-header'>
@@ -45,13 +49,11 @@ const Orders = () => {
             <ul>
               {orders.map((order) => (
                 <li className='listt' key={order.order_id}>
-                  {/* Display order information as needed */}
                   <img className='order-images' src={order.product_image} alt="" />
                   <div className="details"><p className='ordermain'>{order.product_name}</p>
                   <p className='ordermain'>Order {order.order_id}</p>
                   <div className="status">{order.status}</div>
                   <h6>{order.date}</h6></div>
-                  {/* Add more details based on your data structure */}
                 </li>
               ))}
             </ul>
